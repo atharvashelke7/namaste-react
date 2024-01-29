0d11baca15d43fccaa0bf2be6603bf0b090de018
Saurabh@3255 <saurabhpshelke@gmail.com>
2024-01-29 17:44:22 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,27 @@
+
+const parent = React.createElement(
+    
+    "div",
+    {id:"parent"},
+    React.createElement( "div",{id:"child"},
+    [React.createElement("h1", {},"I'm an H1 tag"),
+    React.createElement("h2", {},"I'm an H2 tag"),
+
+    ]),
+    React.createElement( "div",{id:"child2"}, [
+    React.createElement("h1", {},"I'm an H1 tag"),
+    React.createElement("h2", {},"I'm an H2 tag"),
+
+    ]),
+  );
+
+
+
+
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+root.render(parent);
